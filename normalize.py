ff81a502137b470960d8c89791bb1ddd1f8ea230
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2022-07-15 15:20:06 +0300

    change code

--- normalize.py
+++ normalize.py
@@ -1,9 +1,3 @@
-"""
-Функція normalize:
-1. Проводить транслітерацію кирилічного алфавіту на латинський.
-2. Замінює всі символи крім латинських літер, цифр на '_'.
-"""
-
 import re
 
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
@@ -18,6 +12,12 @@ for cyr, lat in zip(CYRILLIC_SYMBOLS, TRANSLATION):
 
 
 def normalize(name: str) -> str:
+    """
+    1. Проводить транслітерацію кирилічного алфавіту на латинський.
+    2. Замінює всі символи крім латинських літер, цифр на '_'.
+    :param name:
+    :return str:
+    """
     t_name = name.translate(TRANS)
     t_name = re.sub(r"\W", "_", t_name)
     return t_name
